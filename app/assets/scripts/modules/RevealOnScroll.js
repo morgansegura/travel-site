aa41b272a5e543441a3ed811ee2a79b006dc54bb
Morgan Segura <morgansegura@gmail.com>
2018-03-22 10:06:23 -0700

    Create ReavealOnScroll

---
+++ app/assets/scripts/modules/RevealOnScroll.js
@@ -0,0 +1,32 @@
+import $ from 'jquery';
+import waypints from '../../../../node_modules/waypoints/lib/noframework.waypoints';
+
+class RevealOnScroll {
+
+	constructor(els, offset) {
+		this.itemsToReveal = els;
+		this.offsetPercentage = offset;
+		this.hideInitially();
+		this.createWaypoints();
+	}
+
+	hideInitially() {
+		this.itemsToReveal.addClass('reveal-item');
+	}
+
+	createWaypoints() {
+		var that = this;
+		this.itemsToReveal.each(function(index, value){
+			var currentItem = this;
+			new Waypoint({
+				element: currentItem,
+				handler: function() {
+					$(currentItem).addClass("reveal-item--is-visible");
+				},
+				offset: that.offsetPercentage
+			});
+		});
+	}
+}
+
+export default RevealOnScroll;
